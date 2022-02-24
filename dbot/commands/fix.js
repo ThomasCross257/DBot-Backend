deed67da725c70df14fd00192935233b3f2f791b
Fabian T <fabiant941@gmail.com>
2022-02-24 00:00:35 -0800

    Replaced subCommand with stringOption Also added minor changes

--- dbot/commands/fix.js
+++ dbot/commands/fix.js
@@ -1,23 +1,22 @@
+// Someone please push this command to discord
+// with deploy-commands.js later
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('fix')
 		.setDescription('Fixes any Twitter link for desktop viewing.')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('link')
-                .setDescription('Enter a link')
-                .addStringOption(option => 
-                    option.setName('url')
-                        .setDescription('URL of Twitter post.')
-                        .setRequired(true))),
+        .addStringOption(option =>                                               
+                option.setName('link')                                              // Removed subcommand and replaced with options
+                    .setDescription('Fixes any Twitter link for desktop viewing')   // ----> No need for subcommand ... String Option is better suited
+                    .setRequired(false)),                                           
 	async execute(interaction) {
+        if(!interaction.isCommand()) return;
 		if(!interaction.guild.available){ 
             await interaction.reply('I can not perform this operation in this guild.');
         }
-
-        if(interaction.subcommand.getString.includes("https://twitter.com")){
+        let link = interaction.options.getString('link');
+        if(link.includes("https://twitter.com")){
             await interaction.reply(interaction.content.replace('https://twitter.com', 'https://fxtwitter.com'));
         }
         else{
