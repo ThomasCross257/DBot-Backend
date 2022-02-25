d439dc9c279327cdeee55fa1978802a637b21b83
mannygregory <34413279+mannygregory@users.noreply.github.com>
2022-02-24 17:56:05 -0800

    Add files via upload

--- dbot/events/ready.js
+++ dbot/events/ready.js
@@ -3,5 +3,17 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		const arrayOfStatus = [`I am in ${client.guilds.cache.size} server(s)!`, 
+                           `Prefix is '~'!`, 
+                           `Dbot!`,
+                            ];
+     let index = 0;
+     setInterval(() => {
+         if(index === arrayOfStatus.length) index = 0;
+         const status = arrayOfStatus[index];
+         client.user.setActivity(status,{type: "PLAYING"}).catch(console.error)
+         index++;
+
+     }, 5000)    
 	},
 };
