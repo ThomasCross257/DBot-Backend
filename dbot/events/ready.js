c20d1e4c0f9f20484b219be9f287660ac786124c
Fabian T <fabiant941@gmail.com>
2022-02-24 19:29:11 -0800

    Fixed 'ready.js'

--- dbot/events/ready.js
+++ dbot/events/ready.js
@@ -1,19 +1,30 @@
+/**
+ * Fabian T. 2/24/22
+ * Fixed commit 'Add files via upload'
+ * ready.js will crash from a Type Error
+ * since client was undefined.
+ * Also please move the rest of the description
+ * into the 'about me' settings in the 
+ * Discord Developer page.
+ */
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		const arrayOfStatus = [`I am in ${client.guilds.cache.size} server(s)!`, 
-                           `Prefix is '~'!`, 
-                           `Dbot!`,
-                            ];
-     let index = 0;
-     setInterval(() => {
-         if(index === arrayOfStatus.length) index = 0;
-         const status = arrayOfStatus[index];
-         client.user.setActivity(status,{type: "PLAYING"}).catch(console.error)
-         index++;
-
-     }, 5000)    
-	},
+		client.user.setActivity(																	// Going to repeat myself here
+			`I am in ${client.guilds.cache.size} server${client.guilds.cache.size > 1 ? 's': ''}!`,	// but it works for now
+			{type: 'PLAYING'},																		// will update later
+		);
+		let interval_id = setInterval((client) => {
+			console.log('setInteveral called');
+			if(!client) {console.log('Client not found clearing interveral!');clearInterval(interval_id); return;}
+			client.user.setActivity(
+				`I am in ${client.guilds.cache.size} server${client.guilds.cache.size > 1 ? 's': ''}!`,
+				{type: 'PLAYING'},
+			);
+			console.log('done');
+		}, 300000, client);	// Set to update every 5 mintues
+	}
 };
