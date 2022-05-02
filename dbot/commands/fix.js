76edfcad5907c7fe2277612fb3840468504200cf
ThomasCross257 <thomascross257@gmail.com>
2022-05-02 11:32:15 -0700

    Update fix.js

--- dbot/commands/fix.js
+++ dbot/commands/fix.js
@@ -10,6 +10,9 @@ module.exports = {
         if(message.includes("https://twitter.com")){
             await interaction.reply(message.replace('https://twitter.com', 'https://fxtwitter.com') + " **(Fixed by D-Bot)**");
         }
+        else if(message.includes("https://mobile.twitter.com")){
+            await interaction.reply(message.replace('https://mobile.twitter.com', 'https://fxtwitter.com') + " **(Fixed by D-Bot)**");
+        }
         else{
             await interaction.reply({content: "This is not a valid twitter URL", ephemeral: true});
         }
