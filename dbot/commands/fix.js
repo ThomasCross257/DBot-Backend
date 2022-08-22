67255eb4fd9bb896f89f2115771b1a6dafe34559
Ph00sant <thomascross257@gmail.com>
2022-08-22 14:59:08 -0700

    Update fix.js

--- dbot/commands/fix.js
+++ dbot/commands/fix.js
@@ -8,11 +8,11 @@ module.exports = {
         const message = interaction.targetMessage.content;
         console.log(message);
         if(message.includes("https://twitter.com")){
-            await interaction.reply(message.replace('https://twitter.com', 'https://vxtwitter.com') + " **(Fixed by D-Bot)**");
-        }// Due to the sudden shutdown of fxtwitter, vxtwitter will substitute as a stand in for now.
-        // Unfortunately, videos will no longer function properly with this change.
+            await interaction.reply(message.replace('https://twitter.com', 'https://fxtwitter.com') + " **(Fixed by D-Bot)**"); 
+		//fxtwitter has been rewritten and made to be more functional.
+        }
         else if(message.includes("https://mobile.twitter.com")){
-            await interaction.reply(message.replace('https://mobile.twitter.com', 'https://vxtwitter.com') + " **(Fixed by D-Bot)**");
+            await interaction.reply(message.replace('https://mobile.twitter.com', 'https://fxtwitter.com') + " **(Fixed by D-Bot)**");
         }
         else{
             await interaction.reply({content: "This is not a valid twitter URL", ephemeral: true});
